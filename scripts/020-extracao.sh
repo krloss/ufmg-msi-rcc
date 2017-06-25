de16f8478f8ef4a230f5185d4e2447518129fefe
Carlos Henrique <krloss@github.com>
2017-06-25 08:17:52 -0300

    extrai enderecos

--- scripts/020-extracao.sh
+++ scripts/020-extracao.sh
@@ -8,3 +8,9 @@ grep '\sLocation="[^"]*\w\{2,\}' /run/shm/Users.xml | sed -r 's/.*\s((Id|Locatio
 sed -r 's/.*"\s\},\s"/site:"GH";/; s/"\s:\s"?/:"/g; s/"?,\s"((id|location):")/";\1/; s/"?\s*}\s*$/";/' fontes/github-users.json > /run/shm/020-gh-users
 
 cat /run/shm/*-users | sed -r 's/.*([Ii]d[:=]"[^"]*").*/\1;&/; s/;([Ii]d[:=]"[^"]*")//; s/([Ii]d|[Ss]ite|[Ll]ocation)[:=]"/"/g' | sed -r 's/\s+/ /g; s/";"/\t/g; s/^"|";*$//g' | sort -u > dados/usuarios
+
+# Enderecos
+unzip fontes/allCountries.zip -d /run/shm/
+awk -F '\t' '($7 ~ /A|P/){print $0}' /run/shm/allCountries.txt | sort -u >  dados/gazetteer
+
+grep '^\w' fontes/geonames_countryInfo.txt | cut -f -2,4,5,9,17 | sort -u > dados/paises
