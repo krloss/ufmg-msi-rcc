ef71c3892b5378658139f5051b4e0c299f865113
Carlos Henrique <krloss@github.com>
2017-06-25 08:02:49 -0300

    wget geonames

--- scripts/010-download.sh
+++ scripts/010-download.sh
@@ -11,3 +11,7 @@ cat ../id_rsa.pub >> keys.txt; git commit -am 'Add key'; git push
 cd ../
 ssh -i ssh_rsa_key -NfL *:8017:dutihr.st.ewi.tudelft.nl:27017 ghtorrent@dutihr.st.ewi.tudelft.nl
 mongoexport -u ghtorrentro -p ghtorrentro -d github -c users -q '{location:/\\S{2,}/}' -f id,location -o github-users.json
+
+wget -b http://download.geonames.org/export/dump/allCountries.zip
+wget -b http://download.geonames.org/export/dump/countryInfo.txt
+wget -b http://download.geonames.org/export/dump/hierarchy.zip
